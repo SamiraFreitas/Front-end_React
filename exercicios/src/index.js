2f7c1ee4523918ce5f6386378e6961a45ab133bb
SamiraFreitas <73719899+SamiraFreitas@users.noreply.github.com>
2021-05-25 15:06:11 -0300

    study_react

--- exercicios/src/index.js
+++ exercicios/src/index.js
@@ -1,26 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-//import BomDia from './components/BomDia'
-
-//ReactDOM.render(<BomDia nome='Samira'/>,  document.getElementById('root'))
-
-//import {BoaTarde, BoaNoite} from './components/Multiplos'
-//components BomDia utilizando 2 componentes 
-/*
-ReactDOM.render (
-    <div> 
-        <BoaTarde nome='Samira' />
-        <BoaNoite nome='Gabriel' />
-
-    </div>
-    , document.getElementById('root')
-)*/
-//components saudacao utilizando classes
+import Primeiro from './components/Primeiro'
+import BomDia from './components/BomDia'
+import {BoaTarde, BoaNoite} from './components/Multiplos'
 import Saudacao from './components/Saudacao'
+import Pai from './components/Pai'
+import Filho from './components/Filho'
+
 ReactDOM.render (
     <div> 
-      <Saudacao tipo="Bom dia" nome="Samira "/>
+        <Primeiro/>
+        <BomDia nome="Samira"/>
+        <BoaTarde nome='Samira' />
+        <BoaNoite nome=' Gabriel' />
+        <Saudacao tipo="Bem Vindo" nome="Samira "/>
+        <Pai nome="Marcelo" sobrenome="Freitas" >
+            <Filho nome="Samira" />
+           {/*} <Filho nome="Marcela" sobrenome='Freitas'/>
+            <Filho nome="Pedro" sobrenome='Freitas'/> */}
+        </Pai>
     </div>
     , document.getElementById('root')
 )
+
+
+
